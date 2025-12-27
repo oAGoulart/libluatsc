21f55f0d9e694148e0f8acb9bf25799ad4dbae6f
Augusto Goulart <26435937+oAGoulart@users.noreply.github.com>
2025-12-27 17:47:44 -0300

    Update build.sh
    Signed-off-by: Augusto Goulart <26435937+oAGoulart@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ gcc -Wall -Wextra -Wpedantic -Wshadow -Wformat=2 \
     -fPIC $CFLAGS --std=c11 -O3 -c $targ.c
 
 if [ "$(printenv 'OS')" = 'Windows_NT' ]; then
-  gcc -shared -lm -o $targ.dll $targ.o $LFLAGS -llua54
+  gcc -shared -lm -o $targ.dll $targ.o $LFLAGS -llua55
 else
   gcc -shared -lm -o $targ.so $targ.o $LFLAGS
 fi
